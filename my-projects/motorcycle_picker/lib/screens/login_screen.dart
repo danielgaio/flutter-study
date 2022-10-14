753646ab5c13455d9a9654b75a2a5500c5350424
Daniel <danielelielgaio@gmail.com>
2022-10-13 22:43:57 -0300

    Added name of the app as a title for the login screen

--- my-projects/motorcycle_picker/lib/screens/login_screen.dart
+++ my-projects/motorcycle_picker/lib/screens/login_screen.dart
@@ -14,13 +14,22 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login Page')),
-      body: CustomButton(
-        onTap: () {
-          context.read<FirebaseAuthMethods>().signInWithGoogle(context);
-        },
-        text: 'Sign In With Google',
-      ),
-    );
+        appBar: AppBar(title: Text('Login')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'Motorcycle Picker',
+              textScaleFactor: 3,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            CustomButton(
+              onTap: () {
+                context.read<FirebaseAuthMethods>().signInWithGoogle(context);
+              },
+              text: 'Sign In With Google',
+            ),
+          ],
+        ));
   }
 }
